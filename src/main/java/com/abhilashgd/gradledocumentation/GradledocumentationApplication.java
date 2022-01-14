26c75f3bdee4da454974b2c82ac51e4613323ac3
abhilashgd <anrewgd@gmail.com>
2022-01-14 19:46:00 +0530

    first commit

---
+++ src/main/java/com/abhilashgd/gradledocumentation/GradledocumentationApplication.java
@@ -0,0 +1,13 @@
+package com.abhilashgd.gradledocumentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradledocumentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradledocumentationApplication.class, args);
+	}
+
+}
